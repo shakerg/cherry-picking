a4766f19f5c4b25bdc8cb89e77105dfdea0daf5c
Shaker Gilbert <shaker+pipboy@7q26dl.onmicrosoft.com>
2025-09-20 16:44:28 -0400

    azure-west: add VM resource and admin auth variables

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -50,3 +50,35 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.pubip.id
   }
 }
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "${var.resource_group_name}-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_B1s"
+
+  storage_os_disk {
+    name              = "osdisk-${var.resource_group_name}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "${var.resource_group_name}-vm"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -9,3 +9,22 @@ variable "resource_group_name" {
   type        = string
   default     = "rg-cherry-west"
 }
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "admin_password" {
+  description = "Admin password for the VM (placeholder)"
+  type        = string
+  default     = "P@ssw0rd-West!"
+  sensitive   = true
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for the VM"
+  type        = string
+  default     = ""
+}
