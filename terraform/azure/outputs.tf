8c6f60e5bca334245deff32f3f9ec793d0ddb3aa
Shaker Gilbert <shaker+pipboy@7q26dl.onmicrosoft.com>
2025-09-20 16:44:37 -0400

    azure-west: add outputs for public IP and VM id

--- terraform/azure/outputs.tf
+++ terraform/azure/outputs.tf
@@ -1,1 +1,11 @@
-// outputs to be added in later commits
+output "resource_group_name" {
+	value = azurerm_resource_group.rg.name
+}
+
+output "public_ip" {
+	value = azurerm_public_ip.pubip.ip_address
+}
+
+output "vm_id" {
+	value = azurerm_virtual_machine.vm.id
+}
