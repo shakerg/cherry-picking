c76167c06274f8b9b96efdf8662edbdd881d3cd2
Shaker Gilbert <shaker+pipboy@7q26dl.onmicrosoft.com>
2025-09-20 15:21:23 -0400

    cherry-pick: add Azure VM resource and outputs

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -52,3 +52,35 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.pubip.id
   }
 }
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = var.vm_name
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_B1s"
+
+  storage_os_disk {
+    name              = "osdisk-${var.vm_name}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = var.vm_name
+    admin_username = "azureuser"
+    admin_password = "P@ssw0rd1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
--- terraform/azure/outputs.tf
+++ terraform/azure/outputs.tf
@@ -1,1 +1,11 @@
-// Outputs will be added after resources are added
+output "resource_group_name" {
+	value = azurerm_resource_group.rg.name
+}
+
+output "public_ip" {
+	value = azurerm_public_ip.pubip.ip_address
+}
+
+output "vm_id" {
+	value = azurerm_virtual_machine.vm.id
+}
