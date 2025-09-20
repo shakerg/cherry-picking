891d356b67dfbdda4dac0fba28cf05b060aa2539
Shaker Gilbert <shaker+pipboy@7q26dl.onmicrosoft.com>
2025-09-20 16:43:40 -0400

    azure-west: add public IP and network interface

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -30,3 +30,23 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.10.1.0/24"]
 }
+
+resource "azurerm_public_ip" "pubip" {
+  name                = "pubip-cherry-west"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-cherry-west"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pubip.id
+  }
+}
