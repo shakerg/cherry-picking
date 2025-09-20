81db33d89473343960e568dbf7dcf599e045a612
Shaker Gilbert <shaker+pipboy@7q26dl.onmicrosoft.com>
2025-09-20 16:43:30 -0400

    azure-west: add network resources (RG, VNet, Subnet)

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -12,4 +12,21 @@ provider "azurerm" {
   features {}
 }
 
-// azure-west branch version: resources will be added across commits
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-cherry-west"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-cherry-west"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
